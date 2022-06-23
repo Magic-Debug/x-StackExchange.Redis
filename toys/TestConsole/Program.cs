48cd0f0c3ed1729b397332cf35aea6e8b93484d5
wjkhappy14@hotmail.com <wjkhappy14@hotmail.com>
2022-06-23 15:22:08 +0800

    UnmanagedMemoryManager

--- toys/TestConsole/Program.cs
+++ toys/TestConsole/Program.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using System.Buffers;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
+using Pipelines.Sockets.Unofficial;
+using Pipelines.Sockets.Unofficial.Arenas;
+using Pipelines.Sockets.Unofficial.Buffers;
 using StackExchange.Redis;
 
 namespace TestConsole
@@ -10,6 +15,27 @@ namespace TestConsole
     {
         public static async Task Main()
         {
+            int sizeOfint = Unsafe.SizeOf<int>();
+            int sizeOfstring = Unsafe.SizeOf<string>();
+            int sizeOfobject = Unsafe.SizeOf<object>();
+            int sizeOffloat = Unsafe.SizeOf<float>();
+
+            Arena arena = new Arena();
+            Sequence<int> seq = arena.Allocate<int>(1024);
+            OwnedArena<int> owned = arena.GetArena<int>();
+
+            ArrayPool<byte> pool = ArrayPool<byte>.Create(4096, 16);
+            ArrayPoolAllocator<byte> arrayPoolAllocator = new ArrayPoolAllocator<byte>(pool);
+            IMemoryOwner<byte> memoryOwner = arrayPoolAllocator.Allocate(512);
+            Memory<byte> buffer = memoryOwner.Memory;
+            buffer = new Memory<byte>(pool.Rent(1024));
+            UnmanagedMemoryManager<byte> unmanagedMemory = new UnmanagedMemoryManager<byte>(buffer.Span);
+            MemoryHandle pin = unmanagedMemory.Pin(64);
+            Sequence<byte> s = new Sequence<byte>(buffer);
+            s.AsReadOnly();
+            Owned<long> owned1 = new Owned<long>(long.MaxValue, (x) => Console.WriteLine(x));
+            owned1.Dispose();
+
             var client = ConnectionMultiplexer.Connect("localhost", new StreamWriter(Path.Combine(Environment.CurrentDirectory, "StackExchange.Redis.log")));
             client.GetDatabase().Ping();
             var db = client.GetDatabase(0);
